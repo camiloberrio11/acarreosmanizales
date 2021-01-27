dbcc9751c9cbf68617eb6b4c637871d6ddbedcee
camiloberrio <camiloberriouribe@gmail.com>
2021-01-26 22:16:31 -0500

    Se quita banner principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import BannerContacto from './components/BannerContacto/BannerContacto';
-import BannerPrincipal from './components/BannerPrincipal/BannerPrincipal';
+// import BannerPrincipal from './components/BannerPrincipal/BannerPrincipal';
 import BotonWhatsapp from './components/BotonWhatsapp/BotonWhatsapp';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
