4e3e8df3dd94a7ee790b993ae5bc2c63e63724b5
camiloberrio <camiloberriouribe@gmail.com>
2021-01-26 22:13:49 -0500

    Se quita banner principal y se coloca header

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import BannerContacto from './components/BannerContacto/BannerContacto';
 import BannerPrincipal from './components/BannerPrincipal/BannerPrincipal';
 import BotonWhatsapp from './components/BotonWhatsapp/BotonWhatsapp';
 import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 import ListadoServicios from './components/ListadoServicios/ListadoServicios';
 import PrincipalesServicios from './components/PrincipalesServicios/PrincipalesServicios';
 
 const App = () => {
   return (
     <div id="main-wrapper">
+      <Header />
       <BotonWhatsapp />
-      <BannerPrincipal />
+      {/* <BannerPrincipal /> */}
       <PrincipalesServicios />
       <BannerContacto />
       <ListadoServicios />
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <div className="header-layout">
+      <div className="sec-heading light mx-auto">
+        <h2>Trasteos - Acarreos Manizales</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
